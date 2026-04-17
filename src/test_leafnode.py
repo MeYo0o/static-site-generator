50540c75f6710c50bb1dcf77e59ac2e5dbbe6a73
Moaz Ahmed <justmeyo2025@gmail.com>
2026-04-17 10:33:07 +0200

    Lesson 6: LeafNode

---
+++ src/test_leafnode.py
@@ -0,0 +1,36 @@
+import unittest
+
+from htmlnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_repr(self):
+        node = LeafNode("p", "Hello, world!", {"class": "primary"})
+        self.assertEqual(
+            node.__repr__(),
+            "LeafNode(p, Hello, world!, {'class': 'primary'})",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
