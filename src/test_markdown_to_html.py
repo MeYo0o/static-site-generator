8354015cf783946d0e32d04b1e67beaf45261b93
Moaz Ahmed <justmeyo2025@gmail.com>
2026-04-17 10:48:49 +0200

    Lesson 15: Block to HTML

---
+++ src/test_markdown_to_html.py
@@ -0,0 +1,102 @@
+import unittest
+from block_markdown import markdown_to_html_node
+
+
+class TestMarkdownToHTML(unittest.TestCase):
+    def test_markdown_to_html_node(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_heading(self):
+        md = """
+# heading
+
+## h2
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>heading</h1><h2>h2</h2></div>",
+        )
+
+    def test_blockquote(self):
+        md = """
+> quote
+> more quote
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>quote more quote</blockquote></div>",
+        )
+
+    def test_ul(self):
+        md = """
+- item 1
+- item 2
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>item 1</li><li>item 2</li></ul></div>",
+        )
+
+    def test_ol(self):
+        md = """
+1. item 1
+2. item 2
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>item 1</li><li>item 2</li></ol></div>",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
